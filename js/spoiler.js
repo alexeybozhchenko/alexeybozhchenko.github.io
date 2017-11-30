ca8250399db1fca9854dd27ff109d5fa413fdc2f
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 06:04:26 +0200

    xd

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
  $('.spoiler_links').click(function(){
- $("div[class^='spoiler_body']").hide('normal');
+
   $(this).parent().children('div.spoiler_body').toggle('normal');
   return false;
  });
