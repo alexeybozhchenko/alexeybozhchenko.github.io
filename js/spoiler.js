028e9c0f5e85da93b9f74e7f53662a3a6bd4127b
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 04:39:09 +0200

    spoiler3

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,8 +1,8 @@
 <script type="text/javascript">
 $(document).ready(function(){
  $('.spoiler_links').click(function(){
-  $("div[class^='spoiler_body']").hide('normal');
   $(this).parent().children('div.spoiler_body').toggle('normal');
+  $("div[class^='spoiler_body']").hide('normal');
   return false;
  });
 });
