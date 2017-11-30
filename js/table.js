45084b213f564f10e39f93046bb701d8b228b298
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 11:31:00 +0200

    asd

--- js/table.js
+++ js/table.js
@@ -1,13 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  var tr = document.getElementsByTagName("tr");
-
-  for (var i = 0; i < tr.length; i++) {
-      tr[i].addEventListener("mouseover", function() {
-          this.style.backgroundColor = "#8888FF";
-      });
-
-      tr[i].addEventListener("mouseout", function() {
-          this.style.backgroundColor = "transparent";
-      });
-  }
+$('table tr').mouseover(function() {
+    $(this).addClass('hovered');
+}).mouseout(function() {
+    $(this).removeClass('hovered');
 });
