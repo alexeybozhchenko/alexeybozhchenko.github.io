3055aa1473ac938acd2779a39f0c3193b7a7b75b
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 08:14:56 +0200

    xd

--- js/spoiler2.js
+++ js/spoiler2.js
@@ -1,11 +1,11 @@
 var flag = 0;
+
+function spoiler() {
 var text = document.getElementById("spoilerText");
 var button = document.getElementById("spoilerButton");
-text.style.height = "0px";
-text.style.overflow = "hidden";
-function spoiler() {            
+               
 if(flag == 0 ) {
-    text.style.height = "100px";
+    text.style.height = "auto";
     text.style.overflow = "inherit";
     flag = 1;	
 }
