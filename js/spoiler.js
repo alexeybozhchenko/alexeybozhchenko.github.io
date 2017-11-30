04e4281fc59615630fad769ef0cd5eb7d16df78e
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 06:02:13 +0200

    xd

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
  $('.spoiler_links').click(function(){
+ $("div[class^='spoiler_body']").hide('normal');
   $(this).parent().children('div.spoiler_body').toggle('normal');
-  $("div[class^='spoiler_body']").hide('normal');
   return false;
  });
 });
