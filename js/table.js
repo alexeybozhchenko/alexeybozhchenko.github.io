e5c2703719b073eeb6077a5c3488496a9cfe3dee
alexeybozhchenko <oleksii.bozhchenko@nure.ua>
2017-11-30 10:49:40 +0200

    das

--- js/table.js
+++ js/table.js
@@ -1,20 +1,13 @@
-function highlight_row() {
-    var table = document.getElementById('display-table');
-    var cells = table.getElementsByTagName('td');
+document.addEventListener("DOMContentLoaded", function(event) { 
+  var tr = document.getElementsByTagName("tr");
 
-    for (var i = 0; i < cells.length; i++) {
-        var cell = cells[i];
-        cell.onmouseover = function () {
-            var rowId = this.parentNode.rowIndex;
+  for (var i = 0; i < tr.length; i++) {
+      tr[i].addEventListener("mouseover", function() {
+          this.style.backgroundColor = "#8888FF";
+      });
 
-            var rowsNotSelected = table.getElementsByTagName('tr');
-            for (var row = 0; row < rowsNotSelected.length; row++) {
-                rowsNotSelected[row].style.backgroundColor = "";
-                rowsNotSelected[row].classList.remove('selected');
-            }
-            var rowSelected = table.getElementsByTagName('tr')[rowId];
-            rowSelected.style.backgroundColor = "yellow";
-            rowSelected.className += " selected";          
-        }
-    }
-} 
+      tr[i].addEventListener("mouseout", function() {
+          this.style.backgroundColor = "transparent";
+      });
+  }
+});
